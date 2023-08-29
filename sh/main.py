223e919465a31a6ea9f980dfa246319465c21f45
Vulcorum <wsemion@gmail.com>
2023-08-29 21:48:35 +0300

    kill screen3

--- sh/main.py
+++ sh/main.py
@@ -60,12 +60,12 @@ def main():
     camera.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
 
     eye_tracker = EyeTracker()
-    screen = Screen(SCREEN_WIDTH, SCREEN_HEIGHT)
+    # screen = Screen(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-    screen.clean()
-    screen.print_title()
-    screen.print_instructions()
-    screen.show()
+    # screen.clean()
+    # screen.print_title()
+    # screen.print_instructions()
+    # screen.show()
 
     quiz = None
     # cv2.namedWindow("frame")
